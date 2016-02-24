e80b56ebceadf97efae7ffd140bb62194371cccb
Ben Archuleta <Ben@benarchuleta.com>
2016-02-24 15:27:00 -0700

    Added GNU

--- extender.sh
+++ extender.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 echo -e "Please enter the user name for the jobs you wish to extend and press enter"
 echo -e "WARNING: This will effect all Jobs relating to this user\n"
 
